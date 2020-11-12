140beba4dda0e23d02d5b49819a4823c5ff8d750
grevicoc <cocabc1@gmail>
2020-11-12 13:36:53 +0700

    adding function isiKonten()

--- webscraper.py
+++ webscraper.py
@@ -9,27 +9,27 @@ artikel_raw = soup.find('div', {'id':'index_tag'})     #ini dia cuma bakal nemui
 
 konten_artikel = artikel_raw.find_all('div', attrs={'class', 'timeline--item'})     #jadi bisa dibilang artikel_raw itu kayak container utama, dan di sini kita masukkin masing-masing konten dari container utama itu ke list konten_artikel
 
+list_of_artikel = []
+
 for konten in konten_artikel:
     penulis_artikel = konten.find('div', attrs={'class', 'user-box col-lg-12 col-md-12 col-sm-12 col-xs-12'}).find('a').text
 
     tanggal_artikel = konten.find('div', attrs={'class', 'user-box col-lg-12 col-md-12 col-sm-12 col-xs-12'}).find('div').text
     
-    konten_artikel = konten.find('h2').find('a').get('href')    #ini baru sampe tahap kita dapetin masing-masing link artikel kedepannya sih tinggal diulang aja caranya, nanti gue lanjut
+    konten_artikel = konten.find('h2').find('a').get('href')    
 
-    print(penulis_artikel + '\n' + tanggal_artikel + '\n' + konten_artikel + '\n')
-    
-'''
-contoh hasilnya:
-Agung Gumelar Ansori
-10 November 2020 | 2 jam lalu
-https://www.kompasiana.com/agunggumela8765/5faa9e8ed541df5b8c724132/selamat-ya-joe-biden-dan-kamala-harris-menang-gokil-dahh
+    tempDict = {'penulis' : penulis_artikel, 'tanggal artikel' : tanggal_artikel, 'link' : konten_artikel}
 
-Shafira Azzahra
-10 November 2020 | 8 jam lalu
-https://www.kompasiana.com/safirasunmi86/5faa414d8ede487a66517a42/kapan-sadarnya-ekhem-mba-mas-semester-7
+    list_of_artikel.append(tempDict)
 
-sekar ardya wardhani
-07 November 2020 | 3 hari lalu
-https://www.kompasiana.com/sekar36615/5fa65dfad541df674f0d8642/trump-vs-biden-siapakah-yang-akan-menang
-    '''
-    
+def isiKonten(link):                                #fungsi untuk ngambil isi dari link
+    source = requests.get(link).text                
+
+    soup = BeautifulSoup(source, 'html.parser')
+
+    konten = soup.find('div', attrs={'class', 'read-content'}).find_all('p')        #di sini kita ambil semua isi konten dari link tersebut dengan cara semua tag html <p> diambil dan disimpan di konten
+
+    for  paragraf in konten:                        #setelah itu, karena tiap index di konten menandakan dia paragraf ke berapa, makanya gue buat loop biar nanti bisa diatur per paragraf
+        print(paragraf.text + '\n')
+
+isiKonten(list_of_artikel[0]['link'])
